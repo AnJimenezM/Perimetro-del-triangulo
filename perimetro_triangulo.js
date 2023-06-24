60d4240ed0750ceca8fe2db7f2fca4c77dbf8286
AnJimenezM <136032517+AnJimenezM@users.noreply.github.com>
2023-06-24 12:36:02 -0500

    commit

---
+++ perimetro_triangulo.js
@@ -0,0 +1,9 @@
+
+
+
+var lado1 = parseFloat(prompt("lado1"));
+var lado2 = parseFloat(prompt("lado2"));
+var lado3 = parseFloat(prompt("lado3"));
+perimetro = lado1 + lado2 + lado3;
+alert(perimetro +"cm");
+
